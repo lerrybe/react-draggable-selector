15c0e095039178d3b7a3ff1940e8c5224117585d
lerrybe <lerry597@gmail.com>
2023-08-04 20:03:51 +0900

    Change Default options

--- src/constant/options.ts
+++ src/constant/options.ts
@@ -8,44 +8,44 @@ export const DEFAULT_DATE_FORMAT = 'M/D';
 export const DEFAULT_WIDTH = '100%';
 export const DEFAULT_HEIGHT = '500px';
 export const DEFAULT_SCROLL_WIDTH = '3px';
-export const DEFAULT_SCROLL_COLOR = '#217af4';
-export const DEFAULT_SCROLL_BG_COLOR = 'rgba(33, 122, 244, .1)';
+export const DEFAULT_SCROLL_COLOR = '#595959';
+export const DEFAULT_SCROLL_BG_COLOR = '#e1e1e1';
 
 /* ABOUT SLOTS */
-export const DEFAULT_ROW_GAP = '5px';
-export const DEFAULT_COLUMN_GAP = '5px';
+export const DEFAULT_ROW_GAP = '3px';
+export const DEFAULT_COLUMN_GAP = '3px';
 export const DEFAULT_SLOT_MIN_WIDTH = '30px';
 export const DEFAULT_SLOT_HEIGHT = '30px';
 export const DEFAULT_SLOT_BG_COLOR = '#f1f1f1';
-export const DEFAULT_SELECTED_SLOT_BG_COLOR = '#a2d7b4';
-export const DEFAULT_HOVERED_SLOT_BG_COLOR = '#5e96ff';
-export const DEFAULT_DISABLED_SLOT_BG_COLOR = '#7e7e7e';
+export const DEFAULT_SELECTED_SLOT_BG_COLOR = '#3a3a3a';
+export const DEFAULT_HOVERED_SLOT_BG_COLOR = '#cbcbcb';
+export const DEFAULT_DISABLED_SLOT_BG_COLOR = '#707070';
 export const DEFAULT_SLOT_BORDER_STYLE = 'none';
-export const DEFAULT_SLOT_BORDER_RADIUS = '0px';
+export const DEFAULT_SLOT_BORDER_RADIUS = '2px';
 
 /* ABOUT COLUMN LABELS */
-export const DEFAULT_COLUMN_LABELS_COLOR = '#000';
+export const DEFAULT_COLUMN_LABELS_COLOR = '#7a7a7a';
 export const DEFAULT_COLUMN_LABELS_BG_COLOR = 'transparent';
 export const DEFAULT_COLUMN_LABELS_MARGIN = '0px';
 export const DEFAULT_COLUMN_LABELS_BORDER_RADIUS = '0px';
-export const DEFAULT_COLUMN_LABELS_FONT_SIZE = '12px';
+export const DEFAULT_COLUMN_LABELS_FONT_SIZE = '15px';
 export const DEFAULT_COLUMN_LABELS_FONT_WEIGHT = 600;
 export const DEFAULT_COLUMN_LABELS_FONT_FAMILY = 'Pretendard-Regular';
-export const DEFAULT_COLUMN_LABELS_HEIGHT = '50px';
-export const DEFAULT_COLUMN_LABEL_PADDING = '0px 10px';
+export const DEFAULT_COLUMN_LABELS_HEIGHT = '36px';
+export const DEFAULT_COLUMN_LABEL_PADDING = '0px';
 export const DEFAULT_COLUMN_LABEL_BORDER_RADIUS = '0px';
 export const DEFAULT_COLUMN_LABEL_BG_COLOR = 'transparent';
 
 /* ABOUT ROW LABELS */
 export const DEFAULT_TIME_FORMAT = 'hh:mm A';
-export const DEFAULT_ROW_LABELS_COLOR = '#000';
-export const DEFAULT_ROW_LABEL_WIDTH = '80px';
+export const DEFAULT_ROW_LABELS_COLOR = '#939393';
+export const DEFAULT_ROW_LABEL_WIDTH = '68px';
 export const DEFAULT_ROW_LABELS_BG_COLOR = 'transparent';
 export const DEFAULT_ROW_LABELS_MARGIN = '0px';
 export const DEFAULT_ROW_LABELS_BORDER_RADIUS = '0px';
 export const DEFAULT_ROW_LABELS_FONT_SIZE = '12px';
-export const DEFAULT_ROW_LABELS_FONT_WEIGHT = 600;
+export const DEFAULT_ROW_LABELS_FONT_WEIGHT = 400;
 export const DEFAULT_ROW_LABELS_FONT_FAMILY = 'Pretendard-Regular';
-export const DEFAULT_ROW_LABEL_PADDING = '0px 10px';
+export const DEFAULT_ROW_LABEL_PADDING = '0px';
 export const DEFAULT_ROW_LABEL_BORDER_RADIUS = '0px';
 export const DEFAULT_ROW_LABEL_BG_COLOR = 'transparent';
